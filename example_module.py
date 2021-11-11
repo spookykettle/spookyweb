8451a554749182f7decfcdf8dbe4816955ae3be2
Pun Rojanamontien <64011588@kmitl.ac.th>
2021-11-11 19:41:28 +0700

    add a new path that receive 2 parameters to test

---
+++ example_module.py
@@ -0,0 +1,3 @@
+
+def getMattaName():
+    return "MAtta"
--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,18 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request
+import example_module as em
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Pun!'
+    return 'Homepage ja'
+
+@app.route('/hello_user')
+def hello_matta():
+    A = request.args.get('A')
+    B = request.args.get('B')
+    text_to_show = f'hello! welcome khun {A} ans khun {B}'
+    return text_to_show
 
