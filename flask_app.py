cb7a627889fb97df2e161f43451d637a46cb8810
spookykettle <92515499+spookykettle@users.noreply.github.com>
2021-11-11 23:09:07 +0700

    Add new path to test (#1)
    * add a new path that receive 2 parameters to test
    * add comments
    * add gitignore
    * testing

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,23 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request
+import example_module as em
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Pun!'
+    return 'Hello from Pun'
+
+@app.route('/hello_user')
+def hello_matta():
+    """An endpoint that print out 2 input parameters A and B
+
+    Returns:
+        str: welcome message for the 2 input parameters
+    """    
+    A = request.args.get('A')
+    B = request.args.get('B')
+    text_to_show = f'hello! welcome khun {A} ans khun {B}'
+    return text_to_show
 
