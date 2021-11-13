c85a6ae015953efb9946a17692dc63459e463757
Pun Rojanamontien <64011588@kmitl.ac.th>
2021-11-13 16:59:05 +0700

    add aaa, bbb

--- flask_app.py
+++ flask_app.py
@@ -10,7 +10,7 @@ def hello_world():
     return 'Hello from Pun'
 
 @app.route('/hello_user')
-def hello_matta():
+def hello_everyone():
     """An endpoint that print out 2 input parameters A and B
 
     Returns:
@@ -21,3 +21,10 @@ def hello_matta():
     text_to_show = f'hello! welcome khun {A} ans khun {B}'
     return text_to_show
 
+@app.route("/aaa")
+def hello_aaa():
+    return "The path is aaa"
+
+@app.route("/bbb")
+def hello_bbb():
+    return "The path is bbb"
